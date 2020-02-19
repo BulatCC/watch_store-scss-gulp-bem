7c7a26f04170b39ef3c24341c33e3ded39fa1501
BulatCC <4134236@gmail.com>
2020-02-19 14:19:09 +0500

    закончил верстку

--- gulpfile.js
+++ gulpfile.js
@@ -76,6 +76,7 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("copy")); //ыы
   gulp.watch("source/*.html", gulp.series("html", "refresh")).on("change", server.reload);
 });
 
